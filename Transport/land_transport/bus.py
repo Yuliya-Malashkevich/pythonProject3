faebc20facc5e16122c2bdc0e2f28f66cf9affac
Yuliya-Malashkevich <3261029n>
2021-03-03 20:10:10 +0300

    b-->B in bus

--- Transport/land_transport/bus.py
+++ Transport/land_transport/bus.py
@@ -1,8 +1,8 @@
 from Transport.land_transport import land_transport
 
-class bus(land_transport.land_transport):
+class Bus(land_transport.land_transport):
     def __init__(self, brand, country_of_origin, color, maximum_speed, year):
       land_transport.land_transport.__init__(self,brand,country_of_origin,color,maximum_speed,year)
 
-bus1=bus("МАЗ-215","Belarus","yellow","96km/h", "2018")
-bus1.diesel_fuel()
+Bus1=Bus("МАЗ-215","Belarus","yellow","96km/h", "2018")
+Bus1.diesel_fuel()
