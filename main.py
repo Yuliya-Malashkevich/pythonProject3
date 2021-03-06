a145506cf38319c01e537927c15b2f67f0d1f075
Yuliya-Malashkevich <3261029n>
2021-03-06 19:10:33 +0300

    changed the lowercase letter to the uppercase

--- main.py
+++ main.py
@@ -10,58 +10,65 @@ class Transport:
 
     def move_in_space(self):
         print("Move in space")
-#
-# class type_of_fuel:
-#      def diesel_fuel(self):
-#         print("diesel fuel")
-#      def gas(self):
-#          print("gas")
-#      def electricity(self):
-#          print("electricity")
-#      def petrol(self):
-#          print("petrol")
-#
-#
-# class underground_transport(type_of_fuel,transport): # подземный
-#     def move_underground(self):
-#         print("Move underground")
-# class metro(underground_transport):
-#     def __init__(self,brand,country_of_origin,color,maximum_speed,year):
-#         super().__init__(brand,country_of_origin,color,maximum_speed,year)
-# metro1=underground_transport("Stadler","Belarus","green","80km/h",2021)
-# metro1.electricity()
-#
-# class land_transport(type_of_fuel,transport,): # наземный
-#     def move_on_the_land(self):
-#         print("Move on the land")
-#
-# class bus(land_transport):
-#     def __init__(self, brand, country_of_origin, color, maximum_speed, year):
-#       super().__init__(brand,country_of_origin,color,maximum_speed,year)
-# bus1=bus("МАЗ-215","Belarus","yellow","96km/h", 2018)
-# bus1.diesel_fuel()
-# class trolleybus(land_transport):
+
+
+class Type_of_fuel:
+    def diesel_fuel(self):
+        print("diesel fuel")
+    def gas(self):
+        print("gas")
+    def electricity(self):
+        print("electricity")
+    def petrol(self):
+        print("petrol")
+
+class Underground_transport(Type_of_fuel,Transport): # подземный
+    def move_underground(self):
+        print("Move underground")
+
+class Metro(Underground_transport):
+    def __init__(self,brand,country_of_origin,color,maximum_speed,year):
+        Underground_transport.__init__(self,brand,country_of_origin,color,maximum_speed,year)
+Metro1=Metro("Stadler","Belarus","green","80km/h",2021)
+Metro1.electricity()
+
+class Land_transport(Type_of_fuel,Transport): # наземный
+    def move_on_the_land(self):
+        print("Move on the land")
+
+class Bus(Land_transport):
+    def __init__(self, brand, country_of_origin, color, maximum_speed, year):
+      Land_transport.__init__(self,brand,country_of_origin,color,maximum_speed,year)
+
+Bus1=Bus("МАЗ-215","Belarus","yellow","96km/h", "2018")
+Bus1.diesel_fuel()
+
+class Trolleybus(Land_transport):
+    def __init__(self, brand, country_of_origin, color, maximum_speed, year):
+        Land_transport.__init__(self,brand, country_of_origin, color, maximum_speed, year)
+Trolleybus1 = Trolleybus("МАЗ-103Т", "Belarus", "blue", "65km/h", 2019)
+Trolleybus1.electricity()
+
+class Tram(Land_transport):
+    def __init__(self, brand, country_of_origin, color, maximum_speed, year):
+        Land_transport.__init__(self,brand, country_of_origin, color, maximum_speed, year)
+
+
+Tram1 = Tram("АКСМ", "Belarus", "silver", "100km/h", 2017)
+Tram1.electricity()
+
+# class Electric_bus(Land_transport):
 #     def __init__(self, brand, country_of_origin, color, maximum_speed, year):
 #         super().__init__(brand, country_of_origin, color, maximum_speed, year)
-# trolleybus1 = trolleybus("МАЗ-103Т", "Belarus", "blue", "65km/h", 2019)
-# trolleybus1.electricity()
-# class tram(land_transport):
-#     def __init__(self, brand, country_of_origin, color, maximum_speed, year):
-#         super().__init__(brand, country_of_origin, color, maximum_speed, year)
-# tram1 = tram("АКСМ", "Belarus", "silver", "100km/h", 2017)
-# tram1.electricity()
-# class electric_bus(land_transport):
-#     def __init__(self, brand, country_of_origin, color, maximum_speed, year):
-#         super().__init__(brand, country_of_origin, color, maximum_speed, year)
-# electric_bus1 = electric_bus("Е433", "Belarus", "yellow", "60km/h", 2017)
-# electric_bus1.electricity()
+# Electric_bus1 = electric_bus("Е433", "Belarus", "yellow", "60km/h", 2017)
+# Electric_bus1.electricity()
 #
-# class route_taxi(land_transport):
+# class Route_taxi(Land_transport):
 #     def __init__(self, brand, country_of_origin, color, maximum_speed, year):
 #         super().__init__(brand, country_of_origin, color, maximum_speed, year)
-# route_taxi1 = route_taxi("Е433", "Belarus", "yellow", "60km/h", 2017)
-# route_taxi1.diesel_fuel()
-# class autotruck(land_transport): #грузовые
+# Route_taxi1 = Route_taxi("Е433", "Belarus", "yellow", "60km/h", 2017)
+# Route_taxi1.diesel_fuel()
+# class Autotruck(Land_transport): #грузовые
 #     pass
 # class passenger_car(land_transport):
 #     pass
